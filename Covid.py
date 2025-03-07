e8600001724090ccf2840a1171d6e6821ffb8958
RoberthRamon <kennedyramonpaladines@hotmail.com>
2025-03-08 00:48:44 +0100

    WIP: The Covid.py file supports reading a local JSON file or a remote JSON file from the GitHub repository.

--- Covid.py
+++ Covid.py
@@ -1,2 +1,39 @@
+import requests
+import json
+import argparse
+from collections import defaultdict
+
+
+def fetch_data(url):
+    # retrieve data from a URL
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  
+        return response.json() 
+    except requests.exceptions.RequestException as e:
+        print(f"Error while retrieving data: {e}")
+        return None
+
+
 def main():
-    print("ciao")
+    #argparse.ArgumentParser: helps manage and interpret the arguments passed to the script from the command line.
+    parser = argparse.ArgumentParser(description="Aggregate COVID-19 cases by region.")
+    #optional argument --fileJSON. Name of the local JSON file containing COVID data.
+    parser.add_argument("--fileJSON", type=str, help="Path to local dpc-covid19-ita-region.json file.")
+    # list of arguments
+    args = parser.parse_args()
+    # python script.py --file dati.json
+
+    url = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-region.json"
+
+    if args.fileJSON:
+        try:
+            with open(args.fileJSON, "r", encoding="utf-8") as f:
+                data = json.load(f)
+        except FileNotFoundError:
+            print(f"Error: The file '{args.fileJSON}' was not found.")
+            return  
+    else:
+        data = fetch_data(url)
+
+    print ("andato a buon fine")    
